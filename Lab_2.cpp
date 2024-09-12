65a20585a71d251d80a3e1eddecb68690dd03c02
Maksym Demchuk <maks27dem@gmail.com>
2024-09-12 21:50:37 +0300

    first master commit

---
+++ Lab_2.cpp
@@ -0,0 +1,34 @@
+﻿/* 
+Lab_02.cpp
+Демчука Максима
+Лабораторна робота № 2.1
+Лінійні програми.
+Варіант 5
+*/
+
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	double a;
+	double z1;
+	double z2;
+
+	cout<< "Введіть a: "<< endl;
+	cin>>a;
+
+	z1 = 1 - 1/4.0 * (sin(2*a) * sin(2*a)) + cos(2*a);
+    z2 = (cos(a) * cos(a)) + (cos(a) * cos(a) * cos(a) * cos(a));
+	cout <<"Перша формула: " << z1 << endl;
+	cout << "Друга формула: " << z2 << endl;
+   
+	return 0;
+}
+
